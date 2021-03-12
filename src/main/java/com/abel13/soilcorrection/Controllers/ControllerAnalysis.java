a35ca6315d2b6f105181560f907c4e7b2894e914
Abel Dutra <abel.dutra@accurate.com.br>
2021-03-11 22:19:28 -0300

    feat: calculate ideal values

--- src/main/java/com/abel13/soilcorrection/Controllers/ControllerAnalysis.java
+++ src/main/java/com/abel13/soilcorrection/Controllers/ControllerAnalysis.java
@@ -9,17 +9,18 @@ import com.abel13.soilcorrection.Models.Analysis;
 import com.abel13.soilcorrection.Models.CultivationSystem;
 import com.abel13.soilcorrection.Models.SoilTexture;
 import com.abel13.soilcorrection.Models.TechnicalManager;
+import jdk.jshell.spi.ExecutionControl;
 
 /**
  *
  * @author abeldutra
  */
 public class ControllerAnalysis {
-    Analysis analysis;
-    TechnicalManager technicalManager;
-    CultivationSystem cultivationSystem;
+    private Analysis analysis;
+    private TechnicalManager technicalManager;
+    private CultivationSystem cultivationSystem;
     
-    SoilTexture soilTexture;
+    private SoilTexture soilTexture;
     
     ControllerAnalysis() {
          //Data provided by view
@@ -32,7 +33,7 @@ public class ControllerAnalysis {
         soilTexture = new SoilTexture(1, "Argiloso", "+40% de argila");
     }
     
-    private double CalulateIdealPhosphor() {
+    public double getIdealPhosphor() {
         switch(soilTexture.getKind()){
             case 1:
                 return 9.0;
@@ -42,4 +43,61 @@ public class ControllerAnalysis {
                 return 0;
         }
     }
+    
+    public double getIdealPotassium() {
+        //=SE(C6=1;"0,35";SE(C6=2;"0,25";""))
+        switch(soilTexture.getKind()){
+            case 1:
+                return 0.35;
+            case 2:
+                return 0.25;
+            default:
+                return 0;
+        }
+    }
+    
+    public double getIdealCalcium() {
+        //=SE(C6=1;"6,0";SE(C6=2;"4,0";""))
+        switch(soilTexture.getKind()){
+            case 1:
+                return 6.0;
+            case 2:
+                return 4.0;
+            default:
+                return 0;
+        }
+    }
+    
+    public double getIdealMagnesium() {
+        //=SE(C6=1;"1,5";SE(C6=2;"1,0";""))
+        switch(soilTexture.getKind()){
+            case 1:
+                return 1.5;
+            case 2:
+                return 1.0;
+            default:
+                return 0;
+        }
+    }
+    
+    public double getIdealSulfur() {
+        //=SE(C6=1;"9,0";SE(C6=2;"6,0";""))
+        switch(soilTexture.getKind()){
+            case 1:
+                return 9.0;
+            case 2:
+                return 6.0;
+            default:
+                return 0;
+        }
+    }
+    
+    public double getIdealAluminum() {
+        return 0;
+    }
+    
+    public double getIdealPotentialAcidity() {
+        return 0;
+    }
+    
 }
